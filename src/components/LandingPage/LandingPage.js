b35e6a9d38843ded5811e2d32740bbb6226df7c6
David Bolin <david@bolin.at>
2019-04-29 08:12:27 -0700

    landing page initial text

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './LandingPage.css';
 
 export default class LandingPage extends React.Component {
   render() {
     return(
-      <div>Some test text</div>
+      <body>
+        <nav role="navigation">
+          <a href="#">Create Account</a>
+          <a href="#">Sign In</a>
+        </nav>
+        <main role="main">
+          <header role="banner">
+            <h1>Automated Personal Wiki</h1>
+          </header>
+          <section>
+            Organize your ideas with a personal wiki. By default, a page will automatically be linked when other pages contain references to the name of the page, eliminating the need to remember to add each individual link.
+          </section>
+        </main>
+      </body>
     );
   }
 }
