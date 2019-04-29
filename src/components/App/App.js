6716e59b22c20757828e9fbbfc989de30bfa3a42
David Bolin <david@bolin.at>
2019-04-29 07:57:59 -0700

    connected app to blank landing page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import LandingPage from '../LandingPage/LandingPage';
 import './App.css';
 
 function App() {
   return (
     <main className='App'>
-
+      <LandingPage exact path={'/'} />
     </main>
   );
 }
