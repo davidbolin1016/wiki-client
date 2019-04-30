8539db8b1fe75396427f0a9d883822f52ba7cade
David Bolin <david@bolin.at>
2019-04-30 14:03:32 -0700

    basics of update page without save

--- src/components/EditPage/EditPage.js
+++ src/components/EditPage/EditPage.js
@@ -1,5 +1,3 @@
-//copied from NewPage, need to change this
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PageApiService from '../../services/page-api-service';
@@ -7,24 +5,41 @@ import PageApiService from '../../services/page-api-service';
 export default class NewPage extends React.Component {
   
   state = {
-    title: 'New Page ' + new Date(),
+    fetchedPage: false,
+    title: '',
     content: ''
+  }
+
+  componentDidMount() {
+    if (!this.state.fetchedPage) {
+      const pageId = this.props.match.params.page;
+      PageApiService.getPage(pageId)
+        .then(page => {
+          const { page_name, page_content } = page[0];
+          this.setState({
+            fetchedPage: true,
+            title: page_name,
+            content: page_content
+          });
+        });
+    }     
+    
   }
 
   cancel = () => {
     this.props.history.goBack();
   }
 
-  save = () => {
-    PageApiService.submitPage({
-      page_name: this.state.title,
-      page_content: this.state.content
-    })
-      .then((res) => {
-        console.log(res);
-        this.props.history.push(`/pages/${res.page_id}`);
-      });
-  }
+  // save = () => {
+  //   PageApiService.submitPage({
+  //     page_name: this.state.title,
+  //     page_content: this.state.content
+  //   })
+  //     .then((res) => {
+  //       console.log(res);
+  //       this.props.history.push(`/pages/${res.page_id}`);
+  //     });
+  // }
 
   changeFields(event) {
     this.setState({
