198679e9d069a5d83eb6337ee7524b751914ebbc
David Bolin <david@bolin.at>
2019-05-07 10:14:21 -0700

    morning work on styling

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,36 +2,59 @@ import React from 'react';
 import './NavBar.css';
 import UserContext from '../../user-context/UserContext';
 import { Link } from 'react-router-dom';
+import Emoji from '../Emoji/Emoji';
 
 export default class NavBar extends React.Component {
   static contextType = UserContext;
 
   render() {
-    let navDiv = (
-      <>
-         <Link to="/register">Create Account</Link>
-         <Link to="/login">Sign In</Link>
-        </>
-    );
+    let navDiv = [
+      <li>
+        <Link to="/register">
+          <Emoji symbol="🔨" />Create Account
+        </Link>
+      </li>,
+      <li>
+        <Link to="/login">
+          <Emoji symbol="🔑" />Sign In
+        </Link>
+      </li>
+    ];
 
     if (this.context.homepage !== '/') {
-      navDiv = (
-        <>
-          <Link to="/logout">Log Out</Link>
-          <Link to="/newpage">New Page</Link>
-          <Link to="/list">Page List</Link>
-        </>
-      );
+      navDiv = [
+          <li>
+            <Link to="/newpage">
+              <Emoji symbol="🆕" />New Page
+            </Link>
+          </li>,
+          <li>
+            <Link to="/list">
+              <Emoji symbol="📖" />All Pages
+            </Link>
+          </li>,
+          <li>
+            <Link to="/logout">
+              <Emoji symbol="🚪" />Logout
+            </Link>
+          </li>,
+      ];
     } 
     return (
       <nav role="navigation">
-        <div>
+        <div className="controls1">
           <strong>
             <Link to={"/"}>Automated Personal Wiki</Link>
           </strong>
         </div>
-        <Link to={this.context.homepage}>Home Page</Link>
+        <ul className="controls2">
+        <li>
+          <Link to={this.context.homepage}>
+            <Emoji symbol="🏠" />Home
+          </Link>
+        </li>
         {navDiv}
+        </ul>
       </nav>
     );
   }
