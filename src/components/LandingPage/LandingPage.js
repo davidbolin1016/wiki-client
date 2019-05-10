e00ee12d3643e6d317c97c9ecb1b40ff9b8ce96c
David Bolin <david@bolin.at>
2019-05-10 07:33:16 -0700

    updated landing page with indications for demo user

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -9,7 +9,7 @@ export default class LandingPage extends React.Component {
           Automated Personal Wiki
         </header>
         <section className="page-content">
-          Organize your ideas with a personal wiki. A link will automatically be generated whenever one page mentions the title of another page, eliminating the need to remember to add links manually.
+          Organize your ideas with a personal wiki. A link will automatically be generated whenever one page mentions the title of another page, eliminating the need to remember to add links manually. Create an account from the navigation bar to get started, or sign in as 'demo-user' with the password 'password1' to view some potential use cases.
         </section>
       </main>
     );
