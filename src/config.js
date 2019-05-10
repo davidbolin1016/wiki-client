b8c031772e4695090b429bd44cc7650ef84612c2
David Bolin <david@bolin.at>
2019-05-10 05:55:28 -0700

    edited for deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-  // API_ENDPOINT: 'https://sheltered-everglades-14095.herokuapp.com/api',
-  API_ENDPOINT: 'http://localhost:8000/api',
+  // Heroku for deployment, localhost for development
+  API_ENDPOINT: 'https://sheltered-everglades-14095.herokuapp.com/api',
+  // API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: 'token-for-wiki-client'
 }
